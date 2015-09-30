f7f5565c9ecbe2b4b653fbd4d72ac191d4cde825
aanguelov <aanguelov@gmail.com>
2015-09-30 12:38:07 +0300

    Initial commit Problem 1

---
+++ CombinatorialAlgorithms/01.Permutations/Permutations.cs
@@ -0,0 +1,43 @@
+﻿namespace _01.Permutations
+{
+    using System;
+    using System.Linq;
+
+    internal class Permutations
+    {
+        private static int numberOfPermutations;
+
+        private static void Main()
+        {
+            int n = 5;
+            int[] arr = Enumerable.Range(1, n).ToArray();
+            Permute(arr, 0);
+            Console.WriteLine("Total permutations: " + numberOfPermutations);
+        }
+
+        private static void Permute(int[] array, int startIndex)
+        {
+            if (startIndex >= array.Length)
+            {
+                Console.WriteLine(string.Join(", ", array));
+                numberOfPermutations++;
+            }
+            else
+            {
+                for (int i = startIndex; i < array.Length; i++)
+                {
+                    int temp = array[i];
+                    array[i] = array[startIndex];
+                    array[startIndex] = temp;
+
+                    //startIndex = i + 1;
+                    Permute(array, startIndex + 1);
+
+                    int tempSecond = array[i];
+                    array[i] = array[startIndex];
+                    array[startIndex] = tempSecond;
+                }
+            }
+        }
+    }
+}
