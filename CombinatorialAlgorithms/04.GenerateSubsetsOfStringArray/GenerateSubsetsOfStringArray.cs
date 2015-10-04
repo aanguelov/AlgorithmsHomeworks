fbad3c9b2956d6cd2e79148c7385cfd65d551d39
aanguelov <aanguelov@gmail.com>
2015-10-04 12:10:32 +0300

    Add problem 4

---
+++ CombinatorialAlgorithms/04.GenerateSubsetsOfStringArray/GenerateSubsetsOfStringArray.cs
@@ -0,0 +1,32 @@
+﻿namespace _04.GenerateSubsetsOfStringArray
+{
+    using System;
+
+    internal class GenerateSubsetsOfStringArray
+    {
+        private static void Main()
+        {
+            var set = new[] { "test", "rock", "fun" };
+            int k = 2;
+
+            string[] arr = new string[k];
+            GenerateSubsets(set, arr, 0, 0);
+        }
+
+        private static void GenerateSubsets(string[] initial, string[] control, int index, int start)
+        {
+            if (index >= control.Length)
+            {
+                Console.WriteLine(string.Join(" ", control));
+            }
+            else
+            {
+                for (int i = start; i < initial.Length; i++)
+                {
+                    control[index] = initial[i];
+                    GenerateSubsets(initial, control, index + 1, i + 1);
+                }
+            }
+        }
+    }
+}
