ace382dc3e9e128ebab3b918ccafb2181e977c00
aanguelov <aanguelov@gmail.com>
2015-10-12 22:40:00 +0300

    First commit problems 1 and 2

---
+++ DynamicProgramming/01.BinomialCoefficients/BinomialCoefficients.cs
@@ -0,0 +1,33 @@
+﻿namespace _01.BinomialCoefficients
+{
+    using System;
+
+    internal class BinomialCoefficients
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Please enter n and k:");
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int result = Binomial(n, k);
+            Console.WriteLine(result);
+        }
+
+        private static int Binomial(int x, int y)
+        {
+            if (y > x - y)
+            {
+                y = x - y;
+            }
+
+            int temp = 1;
+            for (int i = 0; i < y; i++)
+            {
+                temp = temp * (x - i);
+                temp = temp / (i + 1);
+            }
+
+            return temp;
+        }
+    }
+}
---
+++ DynamicProgramming/02.LongestZigZagSubsequence/LongestZigZagSubsequence.cs
@@ -0,0 +1,75 @@
+﻿namespace _02.LongestZigZagSubsequence
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class LongestZigZagSubsequence
+    {
+        private static void Main()
+        {
+            var sequence = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+
+            List<int> subSequence = new List<int>();
+            subSequence.Add(sequence[0]);
+            subSequence.Add(sequence[1]);
+            int last = sequence[1];
+
+            bool greater = sequence[1] > sequence[0];
+
+            for (int i = 2; i < sequence.Length; i++)
+            {
+                if (greater)
+                {
+                    bool reverse = false;
+                    for (int j = i + 1; j < sequence.Length; j++)
+                    {
+                        if (sequence[j] > sequence[i])
+                        {
+                            reverse = true;
+                            break;
+                        }
+                    }
+
+                    if (i == sequence.Length - 1)
+                    {
+                        reverse = true;
+                    }
+
+                    if (reverse && sequence[i] < last)
+                    {
+                        subSequence.Add(sequence[i]);
+                        last = sequence[i];
+                        greater = !greater;
+                    }
+                }
+                else
+                {
+                    bool reverse = false;
+                    for (int j = i + 1; j < sequence.Length; j++)
+                    {
+                        if (sequence[j] < sequence[i])
+                        {
+                            reverse = true;
+                            break;
+                        }
+                    }
+
+                    if (i == sequence.Length - 1)
+                    {
+                        reverse = true;
+                    }
+
+                    if (reverse && sequence[i] > last)
+                    {
+                        subSequence.Add(sequence[i]);
+                        last = sequence[i];
+                        greater = !greater;
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", subSequence));
+        }
+    }
+}
