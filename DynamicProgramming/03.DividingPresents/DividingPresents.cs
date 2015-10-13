d5158cd02c6d293697a4581c52c187d336773d8a
aanguelov <aanguelov@gmail.com>
2015-10-13 17:36:20 +0300

    Add problems 3, 4, 5

---
+++ DynamicProgramming/03.DividingPresents/DividingPresents.cs
@@ -0,0 +1,42 @@
+﻿namespace _03.DividingPresents
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class DividingPresents
+    {
+        private static void Main()
+        {
+            var presents = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+            Array.Sort(presents);
+            Array.Reverse(presents);
+
+            List<int> presentsAlan = new List<int>();
+            List<int> presentsBob = new List<int>();
+            int maxAlan = 0;
+            int maxBob = 0;
+
+            for (int i = 0; i < presents.Length; i++)
+            {
+                if (maxBob < maxAlan)
+                {
+                    maxBob += presents[i];
+                    presentsBob.Add(presents[i]);
+                }
+                else
+                {
+                    maxAlan += presents[i];
+                    presentsAlan.Add(presents[i]);
+                }
+            }
+
+            int diff = Math.Abs(maxBob - maxAlan);
+
+            Console.WriteLine("Difference " + diff);
+            Console.WriteLine("Alan: " + maxAlan + " Bob: " + maxBob);
+            Console.WriteLine(string.Join(" ", presentsAlan));
+            Console.WriteLine(string.Join(" ", presentsBob));
+        }
+    }
+}
---
+++ DynamicProgramming/05.SumWithLimitedCoins/SumWithLimitedCoins.cs
@@ -0,0 +1,51 @@
+﻿namespace _05.SumWithLimitedCoins
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class SumWithLimitedCoins
+    {
+        private static bool[] usedCoins;
+
+        private static HashSet<string> combinations = new HashSet<string>(); 
+
+        private static List<int> comb = new List<int>(); 
+
+        private static void Main()
+        {
+            int[] coins = { 1, 2, 2, 3, 3, 4, 6 };
+            int sum = 6;
+            usedCoins = new bool[coins.Length];
+            GetSumOfLimitedCoins(coins, sum, comb);
+            Console.WriteLine(combinations.Count);
+        }
+
+        private static void GetSumOfLimitedCoins(int[] arr, int sum, List<int> combTemp, int index = 0)
+        {
+            if (combTemp.Sum() == sum)
+            {
+                combinations.Add(string.Join(",", combTemp));
+                return;
+            }
+
+            if (combTemp.Sum() > sum)
+            {
+                return;
+            }
+
+            for (int i = index; i < arr.Length; i++)
+            {
+                if (!usedCoins[i])
+                {
+                    usedCoins[i] = true;
+                    combTemp.Add(arr[i]);
+                    GetSumOfLimitedCoins(arr, sum, combTemp, i);
+
+                    combTemp.Remove(arr[i]);
+                    usedCoins[i] = false;
+                }
+            }
+        }
+    }
+}
