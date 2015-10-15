92300fe3323a118b1ac41de7ce070ea13d5be10d
aanguelov <aanguelov@gmail.com>
2015-10-15 10:02:59 +0300

    Add problem 4 .cs file

---
+++ DynamicProgramming/04.SumWithUnlimitedAmountOFCoins/SumWithUnlimitedAmountOFCoins.cs
@@ -0,0 +1,40 @@
+﻿namespace _04.SumWithUnlimitedAmountOFCoins
+{
+    using System;
+
+    internal class SumWithUnlimitedAmountOfCoins
+    {
+        private static int totalComb;
+
+        private static void Main()
+        {
+            int[] coins = { 1, 2, 5, 10 };
+            int sum = 13;
+            int tempSum = 0;
+            SumCoinsRecursive(coins, sum, tempSum, 0);
+            Console.WriteLine(totalComb);
+        }
+
+        private static void SumCoinsRecursive(int[] arr, int sum, int tempSum, int index)
+        {
+            if (tempSum > sum)
+            {
+                return;
+            }
+
+            if (tempSum == sum)
+            {
+                totalComb++;
+                return;
+            }
+
+            for (int i = index; i < arr.Length; i++)
+            {
+                tempSum += arr[i];
+                SumCoinsRecursive(arr, sum, tempSum, i);
+
+                tempSum -= arr[i];
+            }
+        }
+    }
+}
