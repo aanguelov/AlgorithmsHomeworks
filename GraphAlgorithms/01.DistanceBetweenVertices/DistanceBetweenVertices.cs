36a2809eb2f1859e1af36a8a3af70dd06589a79b
aanguelov <aanguelov@gmail.com>
2015-10-20 11:53:10 +0300

    Add problems 1 and 2

---
+++ GraphAlgorithms/01.DistanceBetweenVertices/DistanceBetweenVertices.cs
@@ -0,0 +1,90 @@
+﻿namespace _01.DistanceBetweenVertices
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class DistanceBetweenVertices
+    {
+        private static Dictionary<int, List<int>> graph = new Dictionary<int, List<int>>(); 
+
+        private static int minDistance = int.MaxValue;
+
+        private static bool hasChildren = false;
+
+        private static void Main()
+        {
+            string graphName = Console.ReadLine();
+            string inputLine = Console.ReadLine();
+
+            while (inputLine != "Distances to find:")
+            {
+                int[] inputArgs =
+                    inputLine.Split(new[] { '-', '>', ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+                int vertex = inputArgs[0];
+
+                if (!graph.ContainsKey(vertex))
+                {
+                    graph[vertex] = new List<int>();
+                    for (int i = 1; i < inputArgs.Length; i++)
+                    {
+                        graph[vertex].Add(inputArgs[i]);
+                    }
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            string distance = Console.ReadLine();
+
+            while (distance != string.Empty)
+            {
+                int startPoint = distance.Split('-').Select(int.Parse).ToArray()[0];
+                int endPoint = distance.Split('-').Select(int.Parse).ToArray()[1];
+
+                List<int> visited = new List<int>();
+                FindPathsDFS(startPoint, endPoint, visited, new List<int>());
+                if (minDistance == int.MaxValue)
+                {
+                    Console.WriteLine("{0} -> {1}", distance, -1);
+                }
+                else
+                {
+                    Console.WriteLine("{0} -> {1}", distance, minDistance);
+                    minDistance = int.MaxValue;
+                }
+
+                distance = Console.ReadLine();
+            }
+        }
+
+        private static void FindPathsDFS(int startVertex, int endVertex, List<int> visited, List<int> path)
+        {
+            if (startVertex == endVertex)
+            {
+                if (path.Count < minDistance)
+                {
+                    minDistance = path.Count;
+                    return;
+                }
+            }
+
+            if (!visited.Contains(startVertex))
+            {
+                visited.Add(startVertex);
+                foreach (var child in graph[startVertex])
+                {
+                    if (!visited.Contains(child))
+                    {
+                        path.Add(child);
+                        FindPathsDFS(child, endVertex, visited, path);
+                        path.Remove(child);
+                        visited.Remove(child);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ GraphAlgorithms/02.AreasInMatrix/AreasInMatrix.cs
@@ -0,0 +1,73 @@
+﻿namespace _02.AreasInMatrix
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class AreasInMatrix
+    {
+        private static bool[,] visited;
+
+        private static void Main()
+        {
+            string matrixRows = Console.ReadLine();
+            int rows = int.Parse(matrixRows.Substring(matrixRows.Length - 1));
+            
+            char[][] matrix = new char[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                matrix[i] = Console.ReadLine().ToCharArray();
+            }
+
+            visited = new bool[rows, matrix[0].Length];
+            var result = new Dictionary<char, int>();
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    if (!visited[row, col])
+                    {
+                        if (!result.ContainsKey(matrix[row][col]))
+                        {
+                            result[matrix[row][col]] = 1;
+                        }
+                        else
+                        {
+                            result[matrix[row][col]]++;
+                        }                       
+                    }
+
+                    FindConnectedAreas(row, col, matrix[row][col], matrix);
+                }
+            }
+
+            Console.WriteLine("Areas: " + result.Values.Sum());
+            foreach (var pair in result)
+            {
+                Console.WriteLine("Letter '{0}' -> {1}", pair.Key, pair.Value);
+            }
+        }
+
+        private static void FindConnectedAreas(int row, int col, char ch, char[][] matrix)
+        {
+            if (row < 0 || col < 0 || row > matrix.GetLength(0) - 1 || col > matrix[row].Length - 1)
+            {
+                return;
+            }
+
+            if (visited[row, col] || matrix[row][col] != ch)
+            {
+                return;
+            }
+
+            visited[row, col] = true;
+
+            FindConnectedAreas(row + 1, col, ch, matrix); // go south
+            FindConnectedAreas(row, col + 1, ch, matrix); // go east
+            FindConnectedAreas(row - 1, col, ch, matrix); // go north
+            FindConnectedAreas(row, col - 1, ch, matrix); // go west
+        }
+    }
+}
