90f1a4ee546f9eafb93771c4153d146d5a87daf1
aanguelov <aanguelov@gmail.com>
2015-10-25 11:02:06 +0200

    Add problem 3 and 4

---
+++ GraphAlgorithms/03.CyclesInAGraph/CyclesInAGraph.cs
@@ -0,0 +1,93 @@
+﻿namespace _03.CyclesInAGraph
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class CyclesInAGraph
+    {
+        private static void Main()
+        {
+            Dictionary<string, List<string>> graph = new Dictionary<string, List<string>>();
+
+            string input = Console.ReadLine();
+
+            while (input != string.Empty)
+            {
+                string node1 = input.Substring(0, 1);
+                string node2 = input.Substring(input.Length - 1);
+
+                if (!graph.ContainsKey(node1))
+                {
+                    graph.Add(node1, new List<string>());                  
+                }
+
+                graph[node1].Add(node2);
+
+                if (!graph.ContainsKey(node2))
+                {
+                    graph.Add(node2, new List<string>());
+                }
+
+                graph[node2].Add(node1);
+
+                input = Console.ReadLine();
+            }
+
+            Dictionary<string, int> predecessors = new Dictionary<string, int>();
+
+            foreach (var key in graph.Keys)
+            {
+                if (!predecessors.ContainsKey(key))
+                {
+                    predecessors.Add(key, 0);
+                }
+
+                foreach (var child in graph[key])
+                {
+                    if (!predecessors.ContainsKey(child))
+                    {
+                        predecessors.Add(child, 0);
+                    }
+
+                    predecessors[child]++;
+                }
+            }
+
+            bool[] isRemoved = new bool[graph.Count];
+            List<string> removedNodes = new List<string>();
+            bool nodeRemoved = true;
+
+            while (nodeRemoved)
+            {
+                nodeRemoved = false;
+                foreach (var key in predecessors.Keys)
+                {
+                    if (predecessors[key] <= 1)
+                    {
+                        foreach (var child in graph[key])
+                        {
+                            if (predecessors.ContainsKey(child))
+                            {
+                                predecessors[child]--;
+                            }
+                        }
+
+                        predecessors.Remove(key);
+                        removedNodes.Add(key);
+                        nodeRemoved = true;
+                        break;
+                    }
+                }
+            }
+
+            if (removedNodes.Count == graph.Count)
+            {
+                Console.WriteLine("Acyclic: Yes");
+            }
+            else
+            {
+                Console.WriteLine("Acyclic: No");
+            }
+        }
+    }
+}
---
+++ GraphAlgorithms/04.Salaries/Salaries.cs
@@ -0,0 +1,67 @@
+﻿namespace _04.Salaries
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Salaries
+    {
+        private static List<int>[] graph; 
+
+        private static Dictionary<int, decimal> salary = new Dictionary<int, decimal>();  
+
+        private static void Main()
+        {
+            int employeesCount = int.Parse(Console.ReadLine());
+
+            graph = new List<int>[employeesCount];
+            var hasParents = new bool[employeesCount];
+
+            for (int i = 0; i < graph.Length; i++)
+            {
+                graph[i] = new List<int>();
+                string input = Console.ReadLine();
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    if (input[j] == 'Y')
+                    {
+                        graph[i].Add(j);
+                        hasParents[j] = true;
+                    }
+                }
+            }
+
+            for (int i = 0; i < employeesCount; i++)
+            {
+                if (!hasParents[i])
+                {
+                    DFSTraversal(i);
+                }               
+            }
+
+            Console.WriteLine(salary.Values.Sum());
+        }
+
+        private static void DFSTraversal(int node)
+        {
+            if (!salary.ContainsKey(node))
+            {
+                salary.Add(node, 0);
+
+                if (graph[node].Count > 0)
+                {
+                    foreach (var child in graph[node])
+                    {
+                        DFSTraversal(child);
+                        salary[node] += salary[child];
+                    }
+                }
+                else
+                {
+                    salary[node]++;
+                }
+            }
+        }
+    }
+}
